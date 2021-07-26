501e438d75afd3010105852a41450e1b9004918e
Vinícius Santos <vinicius.santos.oliv3ira@gmail.com>
2021-07-26 20:27:33 -0300

    create class Estado and Cidade

---
+++ src/main/java/br/com/minhaloja/domain/Estado.java
@@ -0,0 +1,65 @@
+package br.com.minhaloja.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class Estado implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Date instante;
+
+    @OneToMany(mappedBy = "estado")
+    private List<Cidade> cidades;
+
+    public Estado() {
+    }
+
+    public Estado(Integer id, Date instante) {
+        this.id = id;
+        this.instante = instante;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getInstante() {
+        return instante;
+    }
+
+    public void setInstante(Date instante) {
+        this.instante = instante;
+    }
+
+    public List<Cidade> getCidades() {
+        return cidades;
+    }
+
+    public void setCidades(List<Cidade> cidades) {
+        this.cidades = cidades;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Estado estado = (Estado) o;
+        return id.equals(estado.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
