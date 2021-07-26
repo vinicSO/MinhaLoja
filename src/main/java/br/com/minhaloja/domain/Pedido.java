efdf941189c215cab1d7e9b741cc936c3bdf2522
Vinícius Santos <vinicius.santos.oliv3ira@gmail.com>
2021-07-25 21:16:13 -0300

    mapped Produto, Cliente, Pedido and Categoria

--- src/main/java/br/com/minhaloja/domain/Pedido.java
+++ src/main/java/br/com/minhaloja/domain/Pedido.java
@@ -1,12 +1,19 @@
 package br.com.minhaloja.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+@Entity
 public class Pedido implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private Date instante;
 
--- src/main/java/br/com/minhaloja/domain/Produto.java
+++ src/main/java/br/com/minhaloja/domain/Produto.java
@@ -2,6 +2,7 @@ package br.com.minhaloja.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
