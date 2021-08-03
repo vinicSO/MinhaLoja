f218381fe4dff502a051c7e6cacfa4798d89d8c3
Vinicius Santos de Oliveira <vinicius.santos.oliv3ira@gmail.com>
2021-08-03 19:24:18 -0300

    create class Pagamento and enum EstadoPagamento

---
+++ src/main/java/br/com/minhaloja/domain/Pagamento.java
@@ -0,0 +1,71 @@
+package br.com.minhaloja.domain;
+
+import br.com.minhaloja.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Pagamento implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private Integer id;
+
+    private Integer estado;
+
+    @JsonIgnore
+    @JoinColumn(name = "pedido_id")
+    @OneToOne
+    @MapsId
+    private Pedido pedido;
+
+    public Pagamento() {
+    }
+
+    public Pagamento(Integer id, EstadoPagamento estado, Pedido pedido) {
+        this.id = id;
+        this.estado = (estado == null) ? null : estado.getCod();
+        this.pedido = pedido;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Integer estado) {
+        this.estado = estado;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pagamento pagamento = (Pagamento) o;
+        return id.equals(pagamento.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
