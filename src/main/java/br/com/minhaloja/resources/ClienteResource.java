b3239cce7f985dae5c71e2e601ef5c1853a9bf1d
Vinicius Santos de Oliveira <vinicius.santos.oliv3ira@gmail.com>
2021-08-04 19:54:03 -0300

    insert new Cliente with POST method

--- src/main/java/br/com/minhaloja/resources/ClienteResource.java
+++ src/main/java/br/com/minhaloja/resources/ClienteResource.java
@@ -2,12 +2,15 @@ package br.com.minhaloja.resources;
 
 import br.com.minhaloja.domain.AjaxResponseBody;
 import br.com.minhaloja.domain.Cliente;
+import br.com.minhaloja.dto.ClienteNewDTO;
 import br.com.minhaloja.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/clientes")
@@ -16,11 +19,19 @@ public class ClienteResource {
     @Autowired
     private ClienteService clienteService;
 
-    @RequestMapping("/{id}")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<AjaxResponseBody> find(@PathVariable Integer id) {
         AjaxResponseBody<Cliente> result = new AjaxResponseBody();
         Cliente obj = clienteService.find(id);
         result.setObj(obj);
         return ResponseEntity.ok().body(result);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<AjaxResponseBody> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+        Cliente obj = clienteService.fromDTO(objDto);
+        obj = clienteService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/br/com/minhaloja/services/ClienteService.java
+++ src/main/java/br/com/minhaloja/services/ClienteService.java
@@ -1,7 +1,12 @@
 package br.com.minhaloja.services;
 
+import br.com.minhaloja.domain.Cidade;
 import br.com.minhaloja.domain.Cliente;
+import br.com.minhaloja.domain.Endereco;
+import br.com.minhaloja.dto.ClienteNewDTO;
+import br.com.minhaloja.enums.TipoCliente;
 import br.com.minhaloja.repositories.ClienteRepository;
+import br.com.minhaloja.repositories.EnderecoRepository;
 import br.com.minhaloja.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,11 +19,36 @@ public class ClienteService {
     @Autowired
     private ClienteRepository clienteRepository;
 
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
     public Cliente find(Integer id) {
         Optional<Cliente> obj = clienteRepository.findById(id);
 
         return obj.orElseThrow(() -> new ObjectNotFoundException(
                 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()
         ));
+    }
+
+    public Cliente insert(Cliente obj) {
+        obj.setId(null);
+        obj = clienteRepository.save(obj);
+        enderecoRepository.saveAll(obj.getEnderecos());
+        return obj;
+    }
+
+    public Cliente fromDTO(ClienteNewDTO objDto) {
+        Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+        Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+        Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+        cli.getEnderecos().add(end);
+        cli.getTelefones().add(objDto.getTelefone1());
+        if (objDto.getTelefone2()!=null) {
+            cli.getTelefones().add(objDto.getTelefone2());
+        }
+        if (objDto.getTelefone3()!=null) {
+            cli.getTelefones().add(objDto.getTelefone3());
+        }
+        return cli;
     }
 }
