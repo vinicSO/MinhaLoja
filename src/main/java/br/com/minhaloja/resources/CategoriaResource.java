55cc107d88c00ea91fe694676bd01c40f3d43771
Vinicius Santos de Oliveira <vinicius.santos.oliv3ira@gmail.com>
2021-08-04 22:15:03 -0300

    syntactic validation with Bean Validation

--- src/main/java/br/com/minhaloja/resources/CategoriaResource.java
+++ src/main/java/br/com/minhaloja/resources/CategoriaResource.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -39,15 +40,17 @@ public class CategoriaResource {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+    public ResponseEntity<Void> insert(@RequestBody @Valid CategoriaDTO objDto) {
+        Categoria obj = categoriaService.fromDTO(objDto);
         obj = categoriaService.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<AjaxResponseBody> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+    public ResponseEntity<AjaxResponseBody> update(@Valid @RequestBody CategoriaDTO objDto, @PathVariable Integer id) {
         AjaxResponseBody<Categoria> result = new AjaxResponseBody<Categoria>();
+        Categoria obj = categoriaService.fromDTO(objDto);
         obj.setId(id);
         obj = categoriaService.update(obj);
         result.setObj(obj);
--- src/main/java/br/com/minhaloja/services/CategoriaService.java
+++ src/main/java/br/com/minhaloja/services/CategoriaService.java
@@ -2,6 +2,7 @@ package br.com.minhaloja.services;
 
 import br.com.minhaloja.domain.AjaxResponseBody;
 import br.com.minhaloja.domain.Categoria;
+import br.com.minhaloja.dto.CategoriaDTO;
 import br.com.minhaloja.repositories.CategoriaRepository;
 import br.com.minhaloja.services.exceptions.DataIntegrityException;
 import br.com.minhaloja.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
     public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
         PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
         return categoriaRepository.findAll(pageRequest);
+    }
+
+    public Categoria fromDTO(CategoriaDTO objDto) {
+        return new Categoria(objDto.getId(), objDto.getNome());
     }
 }
