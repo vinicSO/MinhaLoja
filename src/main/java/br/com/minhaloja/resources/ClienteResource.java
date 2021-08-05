6329c072e5dec0df9b23d98d645f6e0d3f8bcc6d
Vinicius Santos de Oliveira <vinicius.santos.oliv3ira@gmail.com>
2021-08-04 22:45:36 -0300

    CRUD Cliente with DTO

--- src/main/java/br/com/minhaloja/resources/ClienteResource.java
+++ src/main/java/br/com/minhaloja/resources/ClienteResource.java
@@ -2,15 +2,20 @@ package br.com.minhaloja.resources;
 
 import br.com.minhaloja.domain.AjaxResponseBody;
 import br.com.minhaloja.domain.Cliente;
+import br.com.minhaloja.dto.CategoriaDTO;
+import br.com.minhaloja.dto.ClienteDTO;
 import br.com.minhaloja.dto.ClienteNewDTO;
 import br.com.minhaloja.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/clientes")
@@ -28,10 +33,46 @@ public class ClienteResource {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<AjaxResponseBody> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+    public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
         Cliente obj = clienteService.fromDTO(objDto);
         obj = clienteService.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(value="/{id}", method=RequestMethod.PUT)
+    public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
+        Cliente obj = clienteService.fromDTO(objDto);
+        obj.setId(id);
+        obj = clienteService.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        clienteService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/list", method=RequestMethod.GET)
+    public ResponseEntity<AjaxResponseBody> findAll() {
+        AjaxResponseBody<ClienteDTO> result = new AjaxResponseBody<ClienteDTO>();
+        List<Cliente> list = clienteService.findAll();
+        List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+        result.setResult(listDto);
+        return ResponseEntity.ok().body(result);
+    }
+
+    @RequestMapping(value="/page", method=RequestMethod.GET)
+    public ResponseEntity<AjaxResponseBody> findPage(
+            @RequestParam(value="page", defaultValue="0") Integer page,
+            @RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage,
+            @RequestParam(value="orderBy", defaultValue="nome") String orderBy,
+            @RequestParam(value="direction", defaultValue="ASC") String direction) {
+        AjaxResponseBody<Page<ClienteDTO>> result = new AjaxResponseBody<>();
+        Page<Cliente> list = clienteService.findPage(page, linesPerPage, orderBy, direction);
+        Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
+        result.setObj(listDto);
+        return ResponseEntity.ok().body(result);
     }
 }
--- src/main/java/br/com/minhaloja/services/ClienteService.java
+++ src/main/java/br/com/minhaloja/services/ClienteService.java
@@ -3,14 +3,21 @@ package br.com.minhaloja.services;
 import br.com.minhaloja.domain.Cidade;
 import br.com.minhaloja.domain.Cliente;
 import br.com.minhaloja.domain.Endereco;
+import br.com.minhaloja.dto.ClienteDTO;
 import br.com.minhaloja.dto.ClienteNewDTO;
 import br.com.minhaloja.enums.TipoCliente;
 import br.com.minhaloja.repositories.ClienteRepository;
 import br.com.minhaloja.repositories.EnderecoRepository;
+import br.com.minhaloja.services.exceptions.DataIntegrityException;
 import br.com.minhaloja.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -50,5 +57,39 @@ public class ClienteService {
             cli.getTelefones().add(objDto.getTelefone3());
         }
         return cli;
+    }
+
+    public Cliente update(Cliente obj) {
+        Cliente newObj = find(obj.getId());
+        updateData(newObj, obj);
+        return clienteRepository.save(newObj);
+    }
+
+    public void delete(Integer id) {
+        find(id);
+        try {
+            clienteRepository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+        }
+    }
+
+    public List<Cliente> findAll() {
+        return clienteRepository.findAll();
+    }
+
+    public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return clienteRepository.findAll(pageRequest);
+    }
+
+    public Cliente fromDTO(ClienteDTO objDto) {
+        return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+    }
+
+    private void updateData(Cliente newObj, Cliente obj) {
+        newObj.setNome(obj.getNome());
+        newObj.setEmail(obj.getEmail());
     }
 }
