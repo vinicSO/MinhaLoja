12734ef62c5b37ba26c962d02323f7055d74d700
Vinicius Santos de Oliveira <vinicius.santos.oliv3ira@gmail.com>
2021-08-05 20:26:51 -0300

    endpoint /produtos/list

--- src/main/java/br/com/minhaloja/resources/ProdutoResource.java
+++ src/main/java/br/com/minhaloja/resources/ProdutoResource.java
@@ -1,9 +1,7 @@
 package br.com.minhaloja.resources;
 
 import br.com.minhaloja.domain.AjaxResponseBody;
-import br.com.minhaloja.domain.Categoria;
 import br.com.minhaloja.domain.Produto;
-import br.com.minhaloja.dto.CategoriaDTO;
 import br.com.minhaloja.dto.ProdutoDTO;
 import br.com.minhaloja.services.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import br.com.minhaloja.resources.utils.URL;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/produtos")
@@ -43,6 +42,15 @@ public class ProdutoResource {
         Page<Produto> list = produtoService.search(nomeDecoded, ids, page, linesPerPage, orderBy, direction);
         Page<ProdutoDTO> listDto = list.map(obj -> new ProdutoDTO(obj));
         result.setObj(listDto);
+        return ResponseEntity.ok().body(result);
+    }
+
+    @RequestMapping(value = "/list", method=RequestMethod.GET)
+    public ResponseEntity<AjaxResponseBody> findAll() {
+        AjaxResponseBody<ProdutoDTO> result = new AjaxResponseBody<ProdutoDTO>();
+        List<Produto> list = produtoService.findAll();
+        List<ProdutoDTO> listDto = list.stream().map(obj -> new ProdutoDTO(obj)).collect(Collectors.toList());
+        result.setResult(listDto);
         return ResponseEntity.ok().body(result);
     }
 }
--- src/main/java/br/com/minhaloja/services/ProdutoService.java
+++ src/main/java/br/com/minhaloja/services/ProdutoService.java
@@ -37,4 +37,8 @@ public class ProdutoService {
         List<Categoria> categorias = categoriaRepository.findAllById(ids);
         return produtoRepository.search(nome, categorias, pageRequest);
     }
+
+    public List<Produto> findAll() {
+        return produtoRepository.findAll();
+    }
 }
