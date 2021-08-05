8489bf66408f8971c259dd5ca0d42b1e672978ca
Vinicius Santos de Oliveira <vinicius.santos.oliv3ira@gmail.com>
2021-08-05 18:11:42 -0300

    custom validation to email duplicate

--- src/main/java/br/com/minhaloja/services/validations/ClienteInsertValidator.java
+++ src/main/java/br/com/minhaloja/services/validations/ClienteInsertValidator.java
@@ -1,9 +1,12 @@
 package br.com.minhaloja.services.validations;
 
+import br.com.minhaloja.domain.Cliente;
 import br.com.minhaloja.dto.ClienteNewDTO;
 import br.com.minhaloja.enums.TipoCliente;
+import br.com.minhaloja.repositories.ClienteRepository;
 import br.com.minhaloja.resources.exceptions.FieldMessage;
 import br.com.minhaloja.services.validations.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -11,6 +14,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
 
     @Override
     public void initialize(ClienteInsert ann) {}
@@ -25,6 +31,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
         if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
             list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+        }
+
+        Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+
+        if(aux != null) {
+            list.add(new FieldMessage("email", "Email já está cadastrado"));
         }
 
         for(FieldMessage e : list) {
