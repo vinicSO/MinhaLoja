77791f0cc914c44eb518accdf7a859ae30cafc0e
Vinicius Santos de Oliveira <vinicius.santos.oliv3ira@gmail.com>
2021-08-21 21:27:14 -0300

    send order confirmation by email with html

--- src/main/java/br/com/minhaloja/services/EmailService.java
+++ src/main/java/br/com/minhaloja/services/EmailService.java
@@ -3,9 +3,15 @@ package br.com.minhaloja.services;
 import br.com.minhaloja.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido obj);
 
     void sendEmail(SimpleMailMessage msg);
+
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+    void sendHtmlEmail(MimeMessage msg);
 }
